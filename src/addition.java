c168f45d2ecf8078f193a057ed31a5e2e8f95054
jenifer <jeniferi18022004@gmail.com>
2024-02-01 12:30:17 +0530

    loginpage

---
+++ src/addition.java
@@ -0,0 +1,9 @@
+public  class addition
+{
+    public static void main(String[] args){
+        int a=10;
+        int b=45;
+        int Sum =  a+b;
+        System.out.println(Sum);
+    }
+}
---
+++ src/sub.java
@@ -0,0 +1,9 @@
+public class sub {
+    public static void main(String[] args)
+    {
+        int a=500;
+        int b=345;
+        int sub = a-b;
+        System.out.println(sub);
+    }
+}
